25969ba6e988db4d517130e3c4f369fc39d55ab7
badaty123 <y.m.badat@edu.salford.ac.uk>
2022-04-15 04:57:11 +0100

    Ready For deployment

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path")
+const PORT = process.env.PORT || 5000
 //MiddleWare
 app.use(cors());
 app.use(express.json()); //req.body
 
 
-
+//process . env
+//process .env.NODE_ENV => production or undefined
+if (process.env.NODE_ENV === "production"){
+  //serve static content
+  app.use(express.static(path.join(__dirname,"./client/build")))
+}
+//console.log(__dirname)
 
 //ROUTES//
 
@@ -89,7 +97,11 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 //Listen On Port
-app.listen(5000, () => {
-  console.log("server has started on port 5000");
+app.listen(PORT, () => {
+  console.log(`server has started on port ${PORT}`);
 });
